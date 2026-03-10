1f721092034f6203a6839e8d8f1a3365ff91493a
scorpionenko <151180136+scorpionenko@users.noreply.github.com>
2026-03-10 02:08:14 +0200

    added surveyprodbcontext

--- src/SurveyPro.Web/Program.cs
+++ src/SurveyPro.Web/Program.cs
@@ -1,9 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using SurveyPro.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<SurveyProDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<SurveyProDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
